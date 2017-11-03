bce79e9b39706018fe3e446f037d8f09dd6b0281
Kai <BlueAndOrangeMorality@users.noreply.github.com>
2017-11-03 16:57:06 +0100

    Initial commit

---
+++ Box2dMovement-desktop/src/de/ranagazoo/box2dmovement/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ranagazoo.box2dmovement.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ranagazoo.box2dmovement.Box2dMovement;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Box2dMovement(), config);
+	}
+}
