9bf6acf5665aac38b7a565b1b009c29a025c1f43
Kai <BlueAndOrangeMorality@users.noreply.github.com>
2017-11-10 20:33:56 +0100

    Changed Package

---
+++ Box2dMovement-html/src/de/ranagazoo/box/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package de.ranagazoo.box.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import de.ranagazoo.box.Box2dMovement;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Box2dMovement();
+        }
+}
